23431acb31c0a7fa5bc3aefff2714cd7d719b694
PUNKLANCER <punklancer99@gmail.com>
2020-05-29 15:33:55 +0530

    Updates

---
+++ precache-manifest.e8fce843d9e9fea2de9dd0c940f17644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5a3ff9506d4ac8f15b383bc859f0d7",
+    "url": "/CRWN-CLOTHING/index.html"
+  },
+  {
+    "revision": "f4a84cc7db7bdea1fb43",
+    "url": "/CRWN-CLOTHING/static/css/main.f5f0facf.chunk.css"
+  },
+  {
+    "revision": "ff79748f74dea43053c0",
+    "url": "/CRWN-CLOTHING/static/js/2.d7448137.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/CRWN-CLOTHING/static/js/2.d7448137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a84cc7db7bdea1fb43",
+    "url": "/CRWN-CLOTHING/static/js/main.48a7a0b6.chunk.js"
+  },
+  {
+    "revision": "37bf8b8c64633d7bf1fc",
+    "url": "/CRWN-CLOTHING/static/js/runtime-main.7da543bb.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/CRWN-CLOTHING/static/media/crown.19ef7b17.svg"
+  }
+]);
